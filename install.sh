2b388a7e639f88b372c78229b5566a4e2344672d
Martin Grunwald <martingrunwald@gmail.com>
2024-02-02 21:10:23 +0100

    fix in install script

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 if [[ $1 == "install" ]]; then
-ZDOT="$HOME/.zshell"
-ZSHRC="$HOME/.zshrc"
+  ZDOT="$HOME/.zshell"
+  ZSHRC="$HOME/.zshrc"
 
-cp -r ./zshell "$ZDOT"
+  cp -r ./zshell "$ZDOT"
 
-echo "#!/usr/bin/env zsh" > $ZSHRC
-echo 'export ZDOT='"$ZDOT" >> $ZSHRC
-echo 'source $ZDOT/zshrc.zsh' >> $ZSHRC
+  echo "#!/usr/bin/env zsh" > $ZSHRC
+  echo 'export ZDOT='"$ZDOT" >> $ZSHRC
+  echo 'source $ZDOT/zshrc.zsh' >> $ZSHRC
 else
   cd $HOME
-  git clone https://github.com/grunwmar/zshell.git zshell_install
-  cd ./zshell_install
+  git clone https://github.com/grunwmar/zshell.git $HOME/zshell_install
+  cd $HOME/zshell_install
   sh ./install.sh install
-  cd ..
-  rm -rf ./zshell_install
+  cd $HOME
+  rm -rf $HOME/zshell_install
 fi
