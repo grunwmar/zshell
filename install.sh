c7cf3e6b5ed13849bb4c71b381b0c4de8c13d976
Martin Grunwald <martingrunwald@gmail.com>
2024-02-02 20:58:46 +0100

    fix in install script

--- install.sh
+++ install.sh
@@ -11,9 +11,9 @@ echo 'export ZDOT='"$ZDOT" >> $ZSHRC
 echo 'source $ZDOT/zshrc.zsh' >> $ZSHRC
 else
   cd $HOME
-  git clone https://github.com/grunwmar/zshell.git zshell-install
-  cd zshell-install
+  git clone https://github.com/grunwmar/zshell.git zshell_install
+  cd ./zshell_install
   sh ./install.sh install
   cd ..
-  rm -rf zshell-install
+  rm -rf ./zshell_install
 fi
