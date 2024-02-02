22e5b2bdd453ce330acd2b019cd44152a589e37b
Martin Grunwald <martingrunwald@gmail.com>
2024-02-02 20:48:56 +0100

    install.sh expanded for web installation

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+if not [[ $1 == "web" ]]; then
 ZDOT="$HOME/.zshell"
 ZSHRC="$HOME/.zshrc"
 
@@ -8,3 +9,11 @@ cp -r ./zshell "$ZDOT"
 echo "#!/usr/bin/env zsh" > $ZSHRC
 echo 'export ZDOT='"$ZDOT" >> $ZSHRC
 echo 'source $ZDOT/zshrc.zsh' >> $ZSHRC
+else
+  cd $HOME
+  git clone https://github.com/grunwmar/zshell.git zshell-install
+  cd zshell-install
+  sh ./install.sh
+  cd ..
+  rm -rf zshell-install
+fi
