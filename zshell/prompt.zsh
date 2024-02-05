b1dca8d3637da4864d26f1a9173ff09df3005861
Martin Grünwald <mgrunwal@redhat.com>
2024-02-05 19:06:12 +0100

    icons

--- zshell/prompt.zsh
+++ zshell/prompt.zsh
@@ -4,17 +4,20 @@
 function icon() {
     if [[ $ICONS == "true" ]]; then
       echo "$1"
+      else
+      echo "$2"
     fi
 }
 
-tux_icon=$(icon '🐧 ')
-folder_icon=$(icon '📂 ')
-dollar_icon=$(icon ' 💲')
-clock_icon=$(icon '🕐 ')
-pc_icon=$(icon '💻 ')
-crown_icon=$(icon '👑')
+tux_icon=$(icon '🐧 ' '')
+folder_icon=$(icon ' 📂 ' ':')
+dollar_icon=$(icon '✒️ ' '$')
+clock_icon=$(icon ' 🕐 ' '')
+pc_icon=$(icon ' 💻 ' ' ')
+crown_icon=$(icon ' 👑 ')
+error_icon=$(icon '⚠️  ' '')
 
 PROMPT=$'\n'
-PROMPT+="%(!.[$crown_icon%F{11}root%f].$tux_icon%F{14}%n%f) $folder_icon%F{10}%3~%f "
+PROMPT+="%(!.[$crown_icon%F{11}root%f].$tux_icon%F{14}%n%f)$folder_icon%F{10}%3~%f$dollar_icon "
 
-RPROMPT="%(?.. %F{9}%?%f) $clock_icon%F{12}%T%f $pc_icon%F{4}%m%f"
+RPROMPT="%(?.. $error_icon%F{9}%?%f)$clock_icon%F{12}%T%f$pc_icon%F{4}%m%f"
