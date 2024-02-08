cec49c00f77493b54d30883be690f56e9cad844c
Martin Grunwald <martingrunwald@gmail.com>
2024-02-09 00:13:01 +0100

    Complex commit

--- zshell/prompt.zsh
+++ zshell/prompt.zsh
@@ -6,7 +6,7 @@ precmd() {
   HOSTNAME=$(python3 $PYTHON/strcut.py $ZSH_VAR_HOSTNAME_LENGTH $(hostname))
   PWD_PATH=$(python3 $PYTHON/pwd_decor.py "$PWD")
   PROMPT=$'\n'
-  PROMPT+="%(!.[%F{11}root%f].%F{2}%n%f) $PWD_PATH %(!.#.%%) "
+  PROMPT+="%(!.[%F{11}root%f].%F{14}%n%f) $PWD_PATH %(!.#.%%) "
 
   COLS=$(tput cols)
   if [[ $COLS -ge 70 ]]; then
