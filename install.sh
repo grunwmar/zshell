dd01bf2fa6ff7b1a717896e2ac4179800d7b0fb3
Martin Grunwald <martingrunwald@gmail.com>
2024-02-08 23:51:52 +0100

    Complex commit

--- install.sh
+++ install.sh
@@ -2,6 +2,19 @@
 
   ZDOT="$HOME/.zshell"
   ZSHRC="$HOME/.zshrc"
+  cd $HOME
+
+  echo "Remove old .zshrc and .zshell file and directory (required for intstall)? [y/N] "
+  read answ
+
+  case $answ in
+    "y"|"Y"|"yes"|"Yes"|"YES")
+      rm -rf .zshell .zshrc
+    ;;
+    *)
+      exit
+    ;;
+  esac
 
   cp -r ./zshell "$ZDOT"
 
