b0bb418a8ffa3ffaf825dfdd2c9f6167da44911a
Martin Grunwald <martingrunwald91@gmail.com>
2024-02-13 12:26:58 +0000

    cd runs .auto file

--- zshell/aliases.zsh
+++ zshell/aliases.zsh
@@ -3,14 +3,6 @@
 alias vim="nvim"
 alias ed="vim"
 
-alias tmux="tmux -f $ZDOT/configs/tmux.conf"
-
-if [[ $ZSH_VAR_LS_COLORS == "True" ]]; then
-  alias ls="ls -p --color=always"
-else
-  alias ls="ls -p --color=never"
-fi
-
 alias la="ls -Ap"
 alias ll="ls -lAhp"
 
---
+++ zshell/esntl_aliases.zsh
@@ -0,0 +1,24 @@
+#!/usr/bin/env zsh
+
+alias tmux="tmux -f $ZDOT/configs/tmux.conf"
+
+if [[ $ZSH_VAR_LS_COLORS == "True" ]]; then
+  alias ls="ls -p --color=always"
+else
+  alias ls="ls -p --color=never"
+fi
+
+function cdauto () {
+  if [[ -z ".auto" ]]; then
+    case "$1" in
+      "")
+        cd $HOME
+      ;;
+
+      *)
+        cd "$1"
+      ;;
+    esac
+    zsh .auto
+  fi
+}
