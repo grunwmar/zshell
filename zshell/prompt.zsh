a6a50a7f2fa9646c13859ff1f82e471bd8f66382
Martin Grünwald <mgrunwal@redhat.com>
2024-06-18 14:20:21 +0200

    Simple git and venv enhancement of prompt

--- zshell/prompt.zsh
+++ zshell/prompt.zsh
@@ -4,15 +4,33 @@ PYTHON=$ZDOT/python
 
 precmd() {
 
-  PROMPT=$''
-  PROMPT+="%(!.[%F{11}root%f] .)%F{10}%3~%f%(!.#.%%) "
 
   COLS=$(tput cols)
 
+  if [[ -n $VIRTUAL_ENV_PROMPT ]]; then
+         VPROMPT=:${${VIRTUAL_ENV_PROMPT:gs/)/}:gs/(/}
+  else
+      	VPROMPT=""
+  fi
+
+
+  if [[ -f ".git/HEAD" ]]; then
+        git_head=$(cat .git/HEAD)
+        git_head=$(basename $git_head)
+        git_head="[%F{11}$git_head%f]"
+  else
+      	git_head=""
+  fi
+
+
+  PROMPT=$''
+  PROMPT+="%(!.[%F{11}root%f] .)%F{10}%3~%f$git_head%(!.#.%%) "
+
+
   if [[ $COLS -ge 70 ]]; then
-    RPROMPT="%F{14}$VIRTUAL_ENV_PROMPT%f%(?..%F{9}%? %f)%F{7}%n@%m%f %F{10}%T%f"
+    RPROMPT="%F{14}$VPROMPT%f%(?..%F{9}%? %f)%n@%m %F{10}%T%f"
     else
-    RPROMPT="%F{14}$VIRTUAL_ENV_PROMPT%f%n"
+    RPROMPT="%F{14}$VPROMPTT%f%n"
   fi
 }
 
