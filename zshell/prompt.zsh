f70accaf189c5c3d1607bacb8436aa437d2f62d7
martin grunwald <mgrunwald91@gmail.com>
2024-08-07 15:48:18 +0200

    prompt update

--- zshell/prompt.zsh
+++ zshell/prompt.zsh
@@ -8,7 +8,9 @@ precmd() {
   COLS=$(tput cols)
 
   if [[ -n $VIRTUAL_ENV_PROMPT ]]; then
-         VPROMPT=:${${VIRTUAL_ENV_PROMPT:gs/)/}:gs/(/}
+         VPROMPT=${${VIRTUAL_ENV_PROMPT:gs/)/}:gs/(/}
+         VPROMPT=${(MS)VPROMPT##[[:graph:]]*[[:graph:]]}
+         VPROMPT=":%F{12}"$VPROMPT"%f "
   else
       	VPROMPT=""
   fi
@@ -17,22 +19,33 @@ precmd() {
   if [[ -f ".git/HEAD" ]]; then
         git_head=$(cat .git/HEAD)
         git_head=$(basename $git_head)
-        git_head="[%F{11}$git_head%f]"
+        git_head="|%F{11}$git_head%f"
   else
       	git_head=""
   fi
 
+  PROMPT_NL=$'\n'
+  PROMPT=$PROMPT_NL
 
-  PROMPT=$''
-  PROMPT+="%(!.[%F{11}root%f] .)%F{10}%3~%f$git_head%(!.#.%%) "
 
+  if [[ $COLS -ge 80 ]]; then
+    RPROMPT="[@%F{6}%m%f "
+    RPROMPT+=""$VPROMPT""%F{10}%T%f
+    RPROMPT+="]"
 
-  if [[ $COLS -ge 70 ]]; then
-    RPROMPT="%F{14}$VPROMPT%f%(?..%F{9}%? %f)%n@%m %F{10}%T%f"
+    PROMPT+="[%(!.%F{11}root%f .)%F{14}%n%f%F{6}%f:%F{10}%3~%f$git_head]%(!.#.%%) "
     else
-    RPROMPT="%F{14}$VPROMPTT%f%n"
+    RPROMPT=""
+    PROMPT+="%F{8}@%m%f"${PROMPT_NL}
+    PROMPT+="[%(!.%F{11}root%f .)%F{14}%n%f%F{6}%f:%F{10}%3~%f]${PROMPT_NL}[%F{10}%T%f]%(!.#.%%) "
   fi
+
+
 }
+
+
+
+
 
 precmd
 
